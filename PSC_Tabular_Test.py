139825358c4964c485462413ef8aa11c60b8bf0a
109502518 <127176499+109502518@users.noreply.github.com>
2023-12-05 00:13:35 +0800

    Update PSC_Tabular_Test.py

--- PSC_Tabular_Test.py
+++ PSC_Tabular_Test.py
@@ -4,10 +4,8 @@ from sklearn.preprocessing import MinMaxScaler
 import torch
 import torch.nn as nn
 from sklearn.cluster import KMeans
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import adjusted_mutual_info_score, adjusted_rand_score
 from scipy.optimize import linear_sum_assignment
-from sklearn.manifold import SpectralEmbedding
 
 
 class PSC_Mapping_Model(nn.Module):
@@ -41,21 +39,16 @@ def cluster_acc(y_true, y_pred):
 
 def ParseInput():
     _parser = argparse.ArgumentParser()
-    _parser.add_argument("--ratio", default=0.7, help="Training Data Ratio", type=float)
-    _parser.add_argument("--se", default=0, help="Spectral Embedding", type=int)
-    _parser.add_argument("--epoch", default=100, help="Epoch", type=int)
-    _parser.add_argument("--lr", default=1e-3, help="Learning rate", type=float)
-    _parser.add_argument("--batch", default=15, help="Batch size", type=int)
     _parser.add_argument("--layer1", default=32, help="Hidden Layer 1", type=int)
     _parser.add_argument("--layer2", default=64, help="Hidden Layer 2", type=int)
     _parser.add_argument("--layer3", default=32, help="Hidden Layer 3", type=int)
     args = _parser.parse_args()
-    ratio, se, epochs, lr, batch_size, layer1, layer2, layer3 = args.ratio, args.se, args.epoch, args.lr, args.batch, args.layer1, args.layer2, args.layer3
-    return ratio, se, epochs, lr, batch_size, layer1, layer2, layer3
+    layer1, layer2, layer3 = args.layer1, args.layer2, args.layer3
+    return layer1, layer2, layer3
 
 
 if __name__ == '__main__':
-    ratio, se, epochs, lr, batch_size, layer1, layer2, layer3 = ParseInput()
+    layer1, layer2, layer3 = ParseInput()
     
 
     from sklearn.datasets import load_iris
@@ -68,13 +61,7 @@ if __name__ == '__main__':
     
     scaler = MinMaxScaler()
     X = scaler.fit_transform(X)
-    X_train,X_test,y_train,y_test=train_test_split(X,y,train_size=ratio)
-    
 
-    if se == 0:
-        spectralembedding = SpectralEmbedding(n_components=num_labels, affinity='rbf', gamma=1.0)
-    elif se == 1:
-        spectralembedding = SpectralEmbedding(n_components=num_labels, affinity='nearest_neighbors', n_neighbors=10)
     
     kmeans = KMeans(n_clusters=num_labels, init='random', n_init='auto')
 
@@ -88,7 +75,6 @@ if __name__ == '__main__':
     PSC_index = kmeans.fit_predict(predict_embedding)
 
 
-    # all_embedding = spectralembedding.fit_transform(X)
     all_embedding = torch.load('Tabular_all_embedding').numpy()
     SC_index = kmeans.fit_predict(all_embedding)
 
